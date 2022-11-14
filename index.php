21604795eeb6072f5054ce88348dc527830fc22b
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-11-14 13:56:11 +0400

    Starting up

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chairbine APP</title>
+        <link rel="stylesheet" href="assets/style/default.css">
+    </head>
+    <body>
+    <h1>PHP APP</h1>
+    <?php
+    echo "My first PHP script!";
+    ?>
+
+    </body>
+</html>
